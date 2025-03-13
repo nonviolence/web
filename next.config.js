582e801de96eb99189eed9117347448eb65b75c7
wangwenkui <504556221@qq.com>
2025-03-13 12:36:18 +0800

    Revert "update vercel.json"
    This reverts commit 73a4b04b4b5da4c9bec0e1db33e63b588401faf4.

--- next.config.js
+++ next.config.js
@@ -6,28 +6,6 @@ const nextConfig = {
     formats: ['image/webp'],
     remotePatterns: [],
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
-  },
-  // 增加 Vercel 相关配置
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname,
-  },
-}
-
-// 为 Vercel 环境添加特殊配置
-if (process.env.VERCEL) {
-  Object.assign(nextConfig, {
-    experimental: {
-      ...nextConfig.experimental,
-      serverActions: {
-        ...nextConfig.experimental?.serverActions,
-        timeout: 60000, // 设置为60秒
-      },
-    },
-  });
 }
 
 module.exports = nextConfig 
